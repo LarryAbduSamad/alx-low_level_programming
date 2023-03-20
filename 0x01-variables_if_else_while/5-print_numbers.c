679e1a2e15b8604b3f6f8b769496317c46e7701c
Abdus Samad Larry Abdur Raheem <larryabdusamad@gmail.com>
2023-03-20 13:25:29 +0000

    print base 10 numbers starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - print all base 10 numbers starting from 0
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		printf("%d", number);
+	}
+
+	return (0);
+}
