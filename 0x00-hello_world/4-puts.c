95c12f4235e8ca20ba12eaeb6cbb3b122c011580
Abdus Samad Larry Abdur Raheem <larryabdusamad@gmail.com>
2023-03-21 01:54:16 +0000

    print  string with the puts functions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print a string with the puts function
+* Return: zero on success
+*/
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
