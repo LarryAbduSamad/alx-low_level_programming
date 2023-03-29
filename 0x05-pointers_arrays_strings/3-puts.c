272ddfd839ee51697f2c0e0ad2c2168eb7b8c730
LarryAbduSamad <larryabdusamad@gmail.com>
2023-03-29 05:04:57 +0000

    function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout followed by a new line.
+ * @str: A pointer to the string to be printed.
+ *
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	_putchar(str[i]);
+	i++;
+	}
+
+	_putchar('\n');
+}
